18e0ee5757a6f54047e2da1a54ad8d93e68872c0
AdilSatrio <96171442+AdilSatrio@users.noreply.github.com>
2021-12-16 17:52:27 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import json
-print(st.__version__)
+import altair as alt
 
 DATA = 'data/produksi_minyak_mentah.csv'
 CODE = 'data/kode_negara_lengkap.json'
@@ -28,11 +27,16 @@ negara = st.sidebar.selectbox(
     sorted(data['name'].unique().tolist()),
 )
 
+options = st.sidebar.multiselect(
+    'Pilih Negara Pembanding',
+    sorted(data['name'].unique().tolist()),
+    [])
+
 tahun = st.sidebar.slider(
     'Pilih Tahun', 
     min(data['tahun']), 
     max(data['tahun']), 
-    max(data['tahun'])
+    (min(data['tahun']),max(data['tahun']))
 )
 
 
@@ -43,28 +47,34 @@ chart_data1['tahun'] = pd.to_datetime(chart_data1['tahun'], format='%Y')
 chart_data1 = chart_data1.set_index('tahun')
 chart_data1 = chart_data1.fillna(0)
 
-top10yr = list(sorted(data[data['tahun']==tahun].sort_values('produksi',ascending=False).head(10)['name'].values.tolist() + [negara]))
-chart_data2 = data[(data['tahun']==tahun) & (data['name'].isin(top10yr))][['name','produksi']].sort_values('produksi',ascending=False).set_index('name')
+
+if len(list(set(options+[negara])))>1:
+    chart_data2 = data[(data['tahun']>=tahun[0]) & (data['tahun']<=tahun[-1]) & (data['name'].isin(list(set(options+[negara]))))][['name','tahun','produksi']]
+else:
+    chart_data2 = data[(data['tahun']>=tahun[0]) & (data['tahun']<=tahun[-1])]
 
 top10all = list(sorted(data.groupby('name')['produksi'].sum().sort_values(ascending=False).head(10).reset_index()['name'].values.tolist() + [negara]))
 chart_data3 = data[data['name'].isin(top10all)].groupby('name')['produksi'].sum()
 
-topyear = data[data['tahun']==tahun].sort_values('produksi',ascending=False).drop('tahun',axis=1).iloc[0]
+topyear = data[data['tahun'].isin(list(set(tahun)))].groupby(['name','kode_negara','region','sub-region'])['produksi'].sum().sort_values(ascending=False).reset_index().iloc[0]#.sort_values(['produksi'],ascending=False)#.drop('tahun',axis=1).iloc[0]
 topall = data.groupby(['name','kode_negara','region','sub-region'])['produksi'].sum().sort_values(ascending=False).reset_index().iloc[0]
 
-minyear = data[(data['tahun']==tahun) & (data['produksi']>0)].sort_values('produksi').drop('tahun',axis=1).iloc[0]
+minyear = data[(data['tahun'].isin(list(set(tahun)))) & (data['produksi']>0)].sort_values('produksi').drop('tahun',axis=1).iloc[0]
 minall = data.groupby(['name','kode_negara','region','sub-region'])['produksi'].sum().reset_index()
 minall = minall[minall['produksi']>0].sort_values(by='produksi').iloc[0]
 
-zeroyear = data[(data['tahun']==tahun) & (data['produksi']==0)].reset_index(drop=True).drop(['tahun','produksi'],axis=1)
+zeroyear = data[(data['tahun'].isin(list(set(tahun)))) & (data['produksi']==0)].reset_index(drop=True).drop(['tahun','produksi'],axis=1)
 zeroall = data.groupby(['name','kode_negara','region','sub-region'])['produksi'].sum().reset_index()
 zeroall = zeroall[zeroall['produksi']==0].reset_index(drop=True).drop('produksi',axis=1)
 
-st.header("Produsen Tertinggi dan Terendah")
-cola,colb,colc,cold = st.columns(4)
+st.header("Negara Produsen Minyak")
+cola,colc = st.columns(2)
+
+Y = sorted(set(tahun))
+Y = Y[0] if len(Y)==1 else f"{Y[0]}-{Y[-1]}" 
 
 with cola:
-    st.subheader(f"Produsen Tertinggi {tahun}")
+    st.subheader(f"Tertinggi Selama Tahun {Y}")
     st.components.v1.html(
         f'''
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -80,25 +90,8 @@ with cola:
         '''
     )
 
-with colb:
-    st.subheader(f"Produsen Tertinggi")
-    st.components.v1.html(
-        f'''
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        <div class="card">
-            <p style="margin-bottom:0"><b>Negara : </b>{topall['name']}</p>
-            <p style="margin-bottom:0"><b>Kode Negara : </b>{topall['kode_negara']}</p>
-            <p style="margin-bottom:0"><b>Region : </b>{topall['region']}</p>
-            <p style="margin-bottom:0"><b>Sub-Region : </b>{topall['sub-region']}</p>
-            <p style="margin-bottom:0"><b>Produksi : </b>{topall['produksi']}</p>
-        </div>
-        '''
-    )
-
 with colc:
-    st.subheader(f"Produsen Terendah {tahun}")
+    st.subheader(f"Terendah Selama Tahun {Y}")
     st.components.v1.html(
         f'''
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -114,42 +107,51 @@ with colc:
         '''
     )
 
-with cold:
-    st.subheader(f"Produsen Terendah")
-    st.components.v1.html(
-        f'''
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        <div class="card">
-            <p style="margin-bottom:0"><b>Negara : </b>{minall['name']}</p>
-            <p style="margin-bottom:0"><b>Kode Negara : </b>{minall['kode_negara']}</p>
-            <p style="margin-bottom:0"><b>Region : </b>{minall['region']}</p>
-            <p style="margin-bottom:0"><b>Sub-Region : </b>{minall['sub-region']}</p>
-            <p style="margin-bottom:0"><b>Produksi : </b>{minall['produksi']}</p>
-        </div>
-        '''
+st.header(f"Grafik Time-series Produksi Minyak {negara}")
+
+ts = alt.Chart(chart_data1.reset_index()).mark_area(
+    line={'color':'darkblue'},
+    color=alt.Gradient(
+        gradient='linear',
+        stops=[alt.GradientStop(color='white', offset=0),
+               alt.GradientStop(color='darkblue', offset=1)],
+        x1=1,
+        x2=1,
+        y1=1,
+        y2=0
     )
+).encode(
+    alt.X('tahun:T'),
+    alt.Y('produksi:Q')
+)
 
-st.header("Grafik Time-series")
-st.line_chart(chart_data1)
+st.altair_chart(ts, use_container_width=True)
 
-col1, col2 = st.columns(2)
 
-with col1:
-    st.header(f'Produksi {negara} vs Top 10 Produsen pada Tahun {tahun}')
-    st.bar_chart(chart_data2)
-
-with col2:
-    st.header(f'Produksi {negara} vs Top 10 Produsen Kumulatif')
-    st.bar_chart(chart_data3)
+st.header(f"Perbandingan Jumlah Produksi Minyak")
 
 col3, col4 = st.columns(2)
 
+chart_data2 = chart_data2.fillna(0)
+
+if len(chart_data2['tahun'].unique()) == 1 and options!=[]:
+    gr = alt.Chart(chart_data2).mark_bar().encode(
+        x='name:N',
+        y='produksi:Q',
+    )
+
+else:
+    chart_data2['tahun'] = pd.to_datetime(chart_data2['tahun'], format='%Y')
+    gr = alt.Chart(chart_data2).mark_area().encode(
+        x="tahun:T",
+        y="produksi:Q",
+        color="name:N"
+    )
+
 with col3:
-    st.header(f'Negara dengan Produksi Nol pada Tahun {tahun}')
-    st.dataframe(zeroyear)
+    st.subheader(f'Perbandingan Produksi Tahun {Y}')
+    st.altair_chart(gr, use_container_width=True)
 
 with col4:
-    st.header('Negara Non-produsen Minyak')
-    st.dataframe(zeroall)
+    st.subheader(f'Negara dengan Produksi Nol pada Tahun {Y}')
+    st.dataframe(zeroyear)
